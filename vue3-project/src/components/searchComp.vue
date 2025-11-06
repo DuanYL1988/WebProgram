8d643ee4c337400f1ac4c1db088886823a6d5e3a
dylsweetdeath <dylsweetdeath@gmail.com>
2025-11-07 03:21:59 +0900

    检索条件部分通用组件化(Component)

---
+++ vue3-project/src/components/searchComp.vue
@@ -0,0 +1,94 @@
+<script>
+import request from "@/utils/request";
+export default {
+  data() {
+    return {
+      tableName: this.$route.params.tablename,
+      result: {},
+      condition: {},
+    };
+  },
+  methods: {
+    async init(){
+      this.result = {}
+      this.condition= {}
+      let result = await request.get("/getFilterColumns?tableName="+this.tableName);
+      if(200 == result?.code){
+        this.result = result.data
+        this.$emit('initResult', result.data);
+      }
+    },
+    passCondition() {
+      this.$emit('childParam', this.condition);
+    },
+  },
+  /* 計算屬性 */
+  computed: {
+
+  },
+  /* 初始化 */
+  async mounted(){
+    console.debug(this.$route.params);
+    this.init();
+  },
+  /* 監視器 */
+  watch:{
+    '$route.params.tablename'(newVal) {
+      this.tableName = newVal;
+    },
+    tableName:{
+      handler(newVal, oldVal) {
+        if(newVal !== oldVal) {
+          this.init();
+        }
+      },
+      deep: true
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="block">
+    <div class="inputRow">{{tableName}}</div>
+    <div class="inputRow">
+      <div class="inputCell" :key="`column`+index" v-for="column in result.filterColumns">
+        <span>{{column.colNameCh}}</span>
+        <el-select class="imgSelect" v-if="'select'==column.colInputtype" v-model="condition[column.colCamel]">
+          <el-option value=""></el-option>
+          <el-option class="center" v-for="master in result.direct[column.colCode]" :value="master.code" :label="master.value">
+            <img v-if="''!==master.imgUrl" class="selectIcon" :src="result.config.imgUrl + master.imgUrl"/>{{master.value}}
+          </el-option>
+        </el-select>
+        <el-select v-if="'NAME'==column.colName" filterable v-model="condition[column.colCamel]">
+          <el-option value=""></el-option>
+          <el-option class="center" v-for="master in result.direct[column.colName]" :value="master.code" :label="master.value">
+            {{master.value}}
+          </el-option>
+        </el-select>
+        <el-radio-group v-else-if="'radio'==column.colInputtype" v-model="condition[column.colCamel]">
+          <el-radio-button size="small" v-for="master in result.direct[column.colCode]" :label="master.code" >
+              <img v-if="``!==master.imgUrl" class="selectIcon" :src="result.config.imgUrl + master.imgUrl"/>
+              <span v-else style="font-size:large">{{master.value}}</span>
+          </el-radio-button>
+        </el-radio-group>
+        <el-switch v-else-if="'flag'==column.colInputtype" v-model="condition[column.colCamel]" active-value="1" inactive-value="0"></el-switch>
+        <el-input v-else-if="'text'==column.colInputtype" v-model="condition[column.colCamel]"></el-input>
+      </div>
+    </div>
+    <div class="inputRow">
+      <div class="inputCell">
+      <span>自定义条件</span>
+        <el-input v-model="condition['condition']" style="width:450px!important"></el-input>
+      </div>
+      <div class="inputCell">
+        <el-button type="primary" style="width:80px" @click="passCondition()">检索</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.el-radio-button__inner{padding:5px,5px!important}
+.el-input{width:auto!important}
+</style>
--- vue3-project/src/views/management.vue
+++ vue3-project/src/views/management.vue
@@ -1,41 +1,79 @@
 <script>
+import SearchComp from "../components/searchComp.vue"
 import request from "@/utils/request";
 export default {
+  components: {SearchComp},
   data() {
     return {
       tableName: this.$route.params.tablename,
       result: {},
-      condition: {},
       searchDataList:[],
       pageSize:10,
-			pageNo:1
+			pageNo:1,
+      actionTdWidth:150
     };
+  },
+  /* 計算屬性 */
+  computed: {
+		displayList:function(){
+			let result = []
+			let start = (this.pageNo - 1) * this.pageSize
+			for(let i = start; i < start + this.pageSize; i++) {
+        if(i < this.searchDataList.length) {
+          console.debug(i)
+          result.push(this.searchDataList[i])
+        }
+			}
+			return result
+		},
+    tableWidth:function(){
+      let result = this.actionTdWidth
+      $.each(this.result.filterColumns,(index, column)=>{
+        if (column.colListDisableFlag == "1"){
+          result += parseInt(column.colListWidth)
+        }
+      })
+      console.debug("table width:",result)
+      let style = "width:" + result * 2 + "px"
+      return style;
+    }
+	},
+  /* 初始化 */
+  mounted(){
+    this.init();
+  },
+  /* 監視器 */
+  watch:{
+    '$route.params.tablename'(newVal) {
+      this.tableName = newVal;
+      this.init();
+    },
   },
   methods: {
     async init(){
-      this.result = {}
-      this.condition= {}
+      this.result={}
       this.searchDataList=[]
-      let result = await request.get("/getFilterColumns?tableName="+this.tableName);
-      console.debug(result)
-      if(200 == result?.code){
-        this.result = result.data
-      }
+      this.pageSize=10
+      this.pageNo=1
     },
-    async doSearch(){
-      let keys = Object.keys(this.condition)
+    async doSearch(searchCondition){
+      let keys = Object.keys(searchCondition)
       if(keys.length == 0) {
         alert("请设置检索条件!")
       } else {
-        console.debug(JSON.stringify(this.condition))
-        let response = await request.post("/fireemblem/getList", this.condition);
+        let url = "/" + this.tableName + "/getList"
+        let response = await request.post(url, searchCondition);
         if(200 == response?.code){
           this.searchDataList = response.data
         }
       }
     },
     getFaceImgUrl(rowData) {
-        let imageUrl = this.result.config.imgUrl + rowData.faceImg + rowData.imgName.replace("'","") + "_Face_FC.webp"
+        let imageUrl = ""
+        // 规则
+        if("FIREEMBLEM_HERO" == this.tableName) {
+          imageUrl = this.result.config.imgUrl + rowData.faceImg + rowData.imgName.replace("'","") + "_Face_FC.webp"
+        }
         return imageUrl
     },
     getDirection(codeList,code){
@@ -57,91 +95,13 @@ export default {
         this.pageNo = val
     },
   },
-  /* 計算屬性 */
-  computed: {
-		displayList:function(){
-			let result = []
-			let start = (this.pageNo - 1) * this.pageSize
-			for(let i = start; i < start + this.pageSize; i++) {
-        if(i < this.searchDataList.length) {
-          result.push(this.searchDataList[i])
-        }
-			}
-      console.debug(result)
-			return result
-		},
-    tableWidth:function(){
-      let result = 200
-      $.each(this.result.filterColumns,(index, column)=>{
-        if (column.colListDisableFlag == "1"){
-          result += parseInt(column.colListWidth)
-        }
-      })
-      console.debug("table width:",result)
-      let style = "width:" + result * 2 + "px"
-      return style;
-    }
-	},
-  /* 初始化 */
-  async mounted(){
-    console.debug(this.$route.params);
-    this.init();
-  },
-  /* 監視器 */
-  watch:{
-    '$route.params.tablename'(newVal) {
-      this.tableName = newVal;
-    },
-    tableName:{
-      handler(newVal, oldVal) {
-        if(newVal !== oldVal) {
-          this.init();
-        }
-      },
-      deep: true
-    },
-  }
 };
 </script>
 
 <template>
-  <div class="block">
-    <div class="inputRow">{{tableName}}</div>
-    <div class="inputRow">
-      <div class="inputCell" :key="`column`+index" v-for="column in result.filterColumns">
-        <span>{{column.colNameCh}}</span>
-        <el-select v-if="'select'==column.colInputtype" v-model="condition[column.colCamel]">
-          <el-option value=""></el-option>
-          <el-option class="center" v-for="master in result.direct[column.colCode]" :value="master.code" :label="master.value">
-            <img v-if="''!==master.imgUrl" class="selectIcon" :src="result.config.imgUrl + master.imgUrl"/>{{master.value}}
-          </el-option>
-        </el-select>
-        <el-select v-if="'NAME'==column.colName" filterable v-model="condition[column.colCamel]">
-          <el-option value=""></el-option>
-          <el-option class="center" v-for="master in result.direct[column.colName]" :value="master.code" :label="master.value">
-            {{master.value}}
-          </el-option>
-        </el-select>
-        <el-radio-group v-else-if="'radio'==column.colInputtype" v-model="condition[column.colCamel]">
-          <el-radio-button size="small" v-for="master in result.direct[column.colCode]" :label="master.code" >
-              <img v-if="``!==master.imgUrl" class="selectIcon" :src="result.config.imgUrl + master.imgUrl"/>
-              <span v-else style="font-size:large">{{master.value}}</span>
-          </el-radio-button>
-        </el-radio-group>
-        <el-switch v-else-if="'flag'==column.colInputtype" v-model="condition[column.colCamel]" active-value="1" inactive-value="0"></el-switch>
-        <el-input v-else-if="'text'==column.colInputtype" v-model="condition[column.colCamel]"></el-input>
-      </div>
-    </div>
-    <div class="inputRow">
-      <div class="inputCell">
-      <span>自定义条件</span>
-        <el-input v-model="condition['condition']" style="width:450px!important"></el-input>
-      </div>
-      <div class="inputCell">
-        <el-button type="primary" style="width:80px" @click="doSearch()">检索</el-button>
-      </div>
-    </div>
-  </div>
+  <SearchComp @childParam="doSearch" @initResult="result = $event"/>
+
+  <el-button @click="init()">Test</el-button>
     
   <div v-if="searchDataList.length > 0" :style="tableWidth" class="block">
     <el-table :data="displayList" border >
@@ -150,14 +110,14 @@ export default {
         </el-table-column>
         <!-- 通用一览表示 -->
         <template v-for="column in result.listColumns">
-            <el-table-column v-if="'select'== column.colInputtype || 'radio'== column.colInputtype" :width="column.colListWidth" fixed="left" :label="column.colNameCh">
+            <el-table-column v-if="'select'== column.colInputtype || 'radio'== column.colInputtype" :width="column.colListWidth" :label="column.colNameCh">
                 <template #default="scope">{{getDirection(result.direct[column.colCode],scope.row[column.colCamel])}}</template>
             </el-table-column>
             <el-table-column v-else :width="column.colListWidth" :label="column.colNameCh">
                 <template #default="scope">{{scope.row[column.colCamel]}}</template>
             </el-table-column>
         </template>
-        <el-table-column width="150" label="操作">
+        <el-table-column :width="actionTdWidth" fixed="right" label="操作">
             <template #default="scope"></template>
         </el-table-column>
     </el-table>
@@ -175,8 +135,5 @@ export default {
 </template>
 
 <style scoped>
-.center{display: flex; align-items: center;}
-.selectIcon{width:25px;height:25px;margin-right: 10px;}
-.el-radio-button__inner{padding:5px,5px!important}
-.el-input{width:auto!important}
+
 </style>
